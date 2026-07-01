f1d595ce6cb3cc3c02f64c3d16ee7ed9667832bb
vishnuvardhanp1 <vishnu.vis2007@gmail.com>
2026-07-01 21:04:57 +0530

    Intial project commit

--- src/test/java/stepDefinitions/PriceBrandFilterStep.java
+++ src/test/java/stepDefinitions/PriceBrandFilterStep.java
@@ -1,13 +1,49 @@
 package stepDefinitions;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+
+import base.Base;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import pageObjects.PriceBrandFilterPage;
 
-public class PriceBrandFilterStep {
+
+public class PriceBrandFilterStep extends Base{
+	PriceBrandFilterPage priceBrandFilterPage;
+	WebElement search;
+	WebElement brandsMenu;
+	WebElement selectedBrand;
+	WebElement slider;
+	WebElement price;
+	List<WebElement> brandsList=new ArrayList<>();
+	List<WebElement> pricesList=new ArrayList<>();
+	private static final Logger logger =
+			LogManager.getLogger(PriceBrandFilterStep.class);
 	@When("User searches for {string}")
 	public void user_searches_for(String string) {
 	    // Write code here that turns the phrase above into concrete actions
-	    System.out.println("user searches for String");
+		try {
+		 	Thread.sleep(5000);
+		 	getDriver().get(this.getFrameworkUrl());
+		    Thread.sleep(5000);
+		    priceBrandFilterPage=new PriceBrandFilterPage(getDriver());
+		    priceBrandFilterPage.getSearchBar().sendKeys(string, Keys.ENTER);
+		    Thread.sleep(5000);
+
+	        logger.info("Title : " + getDriver().getTitle());
+	        logger.info("URL : " + getDriver().getCurrentUrl());
+	}
+	catch(Exception ex)
+	{
+		System.out.println(ex.getMessage());
+	logger.debug("Exception occurred:"+ex.getMessage());
+	}
 	}
 	@When("User applies the maximum price filter as {string}")
 	public void user_applies_the_maximum_price_filter_as(String string) {
